99b291ed24c5c60a1f03431815c36a5393335854
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2021-11-10 01:56:44 +0530

    ft: decode names in test

--- test/test.js
+++ test/test.js
@@ -2,17 +2,23 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 const candidateArray = ["Daniil Dubov", "Levon Aronian", "Nihal Sarin"];
-describe("Ballot", function () {
-  it("Should get candidates", async function () {
+it("Should add and get candidates and display their names ", async function () {
+  describe("Ballot", function () {
     const Ballot = await ethers.getContractFactory("Ballot");
     const ballot = await Ballot.deploy();
     await ballot.deployed();
 
+    // Add candidates
     candidateArray.forEach(async (name) => {
       await ballot.addCandidate(ethers.utils.formatBytes32String(name));
     });
 
-    const response = await ballot.getCandidates();
-    console.log("Response from getCandidates:", response);
+    const candidates = await ballot.getCandidates();
+    console.log("Response from getCandidates:", candidates);
+
+    const names = candidates.map((candidate) =>
+      ethers.utils.parseBytes32String(candidate.name)
+    );
+    console.log("Names: ", names);
   });
 });
