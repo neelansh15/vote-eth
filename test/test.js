58cc5d4373d16b7faf0c71d32099697aadfdcb15
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2021-11-10 23:29:19 +0530

    fix: test function definition

--- test/test.js
+++ test/test.js
@@ -2,8 +2,8 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 const candidateArray = ["Daniil Dubov", "Levon Aronian", "Nihal Sarin"];
-it("Should add and get candidates and display their names ", async function () {
-  describe("Ballot", function () {
+describe("Ballot Candidates", function () {
+  it("Should add and get candidates and display their names ", async function () {
     const Ballot = await ethers.getContractFactory("Ballot");
     const ballot = await Ballot.deploy();
     await ballot.deployed();
